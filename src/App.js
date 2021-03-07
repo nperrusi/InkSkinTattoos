d779c1d96815cfefef2dd4cf975c444d425d4791
Nicolas Perrusi <nicolas_perrusi@hotmail.com>
2021-03-07 02:25:16 -0300

    commit inicial clase02app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Próximamente Ink Skin Tattoos
+        </p>
+        <a
+          className="App-link"
+          href="http://www.tatuajesgeniales.com/wp-content/uploads/2019/12/tatuajes-maorie-cultura-4.jpg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mostramos tu historia
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
